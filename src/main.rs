affd238899241a58c12532ba63d0648c22f0a3f3
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-01-06 15:22:40 -0800

    accept input file as command line argument

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod types;
 mod owl;
 mod disjoint_sets;
 use crate::owl::Reasoner;
+use std::env;
 
 #[allow(dead_code)]
 const RDFS_SUBCLASSOF: &str = "http://www.w3.org/2000/01/rdf-schema#subClassOf";
@@ -19,34 +20,38 @@ const RDF_TYPE: &str = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 const OWL_INVERSEOF: &str = "http://www.w3.org/2002/07/owl#inverseOf";
 
 fn main() {
-
+    let args: Vec<String> = env::args().collect();
     let mut r = Reasoner::new();
 
     // TODO: load in datasets
     r.load_file("rdfs.ttl").unwrap();
     // Brick.ttl has some parse error so we use n3
     r.load_file("Brick.n3").unwrap();
-    r.load_file("example.n3").unwrap();
+    if args.len() > 1 {
+        r.load_file(args.get(1).unwrap()).unwrap();
+    } else {
+        r.load_file("example.n3").unwrap();
+    }
 
-    let v1 : Vec::<(&str, &str, &str)> = vec![
-        ("a", RDF_TYPE, "Class1"),
-        ("b", RDF_TYPE, "Class1"),
-        ("Class1", RDFS_SUBCLASSOF, "Class2"),
-        ("Class3", RDFS_SUBCLASSOF, "Class2"),
-        ("https://brickschema.org/schema/1.1.0/Brick#feeds", RDFS_DOMAIN, "Class2"),
-        ("https://brickschema.org/schema/1.1.0/Brick#feeds", RDFS_DOMAIN, "Class3"),
-        ("https://brickschema.org/schema/1.1.0/Brick#isFedBy",OWL_INVERSEOF, "https://brickschema.org/schema/1.1.0/Brick#feeds"),
-        ("c", "https://brickschema.org/schema/1.1.0/Brick#feeds", "d"),
+    // let v1 : Vec::<(&str, &str, &str)> = vec![
+    //     ("a", RDF_TYPE, "Class1"),
+    //     ("b", RDF_TYPE, "Class1"),
+    //     ("Class1", RDFS_SUBCLASSOF, "Class2"),
+    //     ("Class3", RDFS_SUBCLASSOF, "Class2"),
+    //     ("https://brickschema.org/schema/1.1.0/Brick#feeds", RDFS_DOMAIN, "Class2"),
+    //     ("https://brickschema.org/schema/1.1.0/Brick#feeds", RDFS_DOMAIN, "Class3"),
+    //     ("https://brickschema.org/schema/1.1.0/Brick#isFedBy",OWL_INVERSEOF, "https://brickschema.org/schema/1.1.0/Brick#feeds"),
+    //     ("c", "https://brickschema.org/schema/1.1.0/Brick#feeds", "d"),
 
-        // cls-thing
-        ("owl:Thing", "rdf:type", "owl:Class"),
-        // cls-nothing1
-        ("owl:Nothing", "rdf:type", "owl:Class"),
+    //     // cls-thing
+    //     ("owl:Thing", "rdf:type", "owl:Class"),
+    //     // cls-nothing1
+    //     ("owl:Nothing", "rdf:type", "owl:Class"),
 
-        // owl definitions
-        ("owl:inverseOf", "rdf:type", "owl:SymmetricProperty"),
-    ];
-    r.load_triples(v1);
+    //     // owl definitions
+    //     ("owl:inverseOf", "rdf:type", "owl:SymmetricProperty"),
+    // ];
+    // r.load_triples(v1);
 
     r.reason();
     r.dump_file("output.n3").unwrap();
