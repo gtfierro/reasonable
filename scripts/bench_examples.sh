e4eb744222b64409c95a3e213ab08fcd52e62b9a
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-01-08 16:02:46 -0800

    benchmark script outputs bench.txt

--- scripts/bench_examples.sh
+++ scripts/bench_examples.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+mv bench.txt bench.txt-old
 export RUST_LOG=info
 ontologies=$(ls example_models/ontologies)
 for file in $(ls example_models/*.n3); do
+    REASONABLE_START=$(date +%s.%N)
     ./target/release/reasonable $ontologies $file
+    REASONABLE_END=$(date +%s.%N)
+    REASONABLE_DIFF=$(echo "$REASONABLE_END - $REASONABLE_START" | bc)
+
+    OWLRL_START=$(date +%s.%N)
     ./scripts/reason_owlrl.py $ontologies $file
+    OWLRL_END=$(date +%s.%N)
+    OWLRL_DIFF=$(echo "$OWLRL_END - $OWLRL_START" | bc)
+
+    SPEEDUP=$(echo "$OWLRL_DIFF / $REASONABLE_DIFF" | bc)
+
+    # cat >>bench.txt << EOF
+    cat <<EOF >>bench.txt
+$file  $REASONABLE_DIFF  $OWLRL_DIFF  $SPEEDUP
+EOF
 done
