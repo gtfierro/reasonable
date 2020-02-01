3d397bffb51beb6c0236c8d09bfeab8ae74a7ba8
Gabe Fierro <gtfierro225@gmail.com>
2020-02-01 11:15:49 -0800

    changing to lib+bin

---
+++ src/bin/reasonable.rs
@@ -0,0 +1,18 @@
+use std::env;
+use reasonable::owl::Reasoner;
+use std::time::Instant;
+use log::info;
+
+fn main() {
+    env_logger::init();
+    let mut r = Reasoner::new();
+    env::args().skip(1).map(|filename| {
+        info!("Loading file {}", &filename);
+        r.load_file(&filename).unwrap()
+    }).count();
+    let reasoning_start = Instant::now();
+    info!("Starting reasoning");
+    r.reason();
+    info!("Reasoning completed in {:.02}sec", reasoning_start.elapsed().as_secs_f64());
+    r.dump_file("output.ttl").unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+extern crate rdf;
+extern crate itertools;
+extern crate log;
+
+mod index;
+pub mod owl;
+mod disjoint_sets;
+#[allow(dead_code)]
+mod common;
