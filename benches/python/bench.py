6a9432720117c0a96ac6116f55def96adee91c3c
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-08-16 23:03:59 -0700

    benchmark stuff

--- benches/python/bench.py
+++ benches/python/bench.py
@@ -1,4 +1,6 @@
 import rdflib
+import glob
+import brickschema
 import time
 
 def load_file(g, f):
@@ -13,39 +15,47 @@ ontology_files = [
     "../../example_models/ontologies/rdfs.ttl",
 ]
 
-data_files = [
-    "../../example_models/small1.n3",
-    "../../example_models/soda_hall.n3",
-]
 
-N = 100
+# data_files = [
+#     "../../example_models/small1.n3",
+#     "../../example_models/soda_hall.n3",
+# ]
+data_files = glob.glob("mortarmodels/*.ttl")
+data_sizes = {}
+for f in data_files:
+    g = rdflib.Graph()
+    g.parse(f, format='ttl')
+    data_sizes[f] = len(g)
+
+N = 10
 
 all_samples = {
     'owlrl': {},
-    'owlready2': {},
+    'allegro': {},
+    # 'owlready2': {},
     'reasonable': {},
 }
 
 
-# owlready2
-import owlready2
-for data_file in data_files:
-    print(f"Benching owlready2 on {data_file}")
-    all_samples['owlready2'][data_file] = []
-    for i in range(N):
-        g = rdflib.Graph()
-        load_file(g, data_file)
-        for ont_file in ontology_files:
-            load_file(g, ont_file)
-        g.serialize("_owlready2_input.ttl", format="ttl")
-        world = owlready2.World()
-        onto = world.get_ontology(f"file://./_owlready2_input.ttl").load()
-        t0 = time.time()
-        owlready2.sync_reasoner(world, infer_property_values =True)
-        G = world.as_rdflib_graph()
-        t1 = time.time()
-        print(f"    owlready2: {data_file} took {t1-t0}")
-        all_samples['owlready2'][data_file].append({'duration': t1-t0, 'triples': len(G)})
+# # owlready2
+# import owlready2
+# for data_file in data_files:
+#     print(f"Benching owlready2 on {data_file}")
+#     all_samples['owlready2'][data_file] = []
+#     for i in range(N):
+#         g = rdflib.Graph()
+#         load_file(g, data_file)
+#         for ont_file in ontology_files:
+#             load_file(g, ont_file)
+#         g.serialize("_owlready2_input.ttl", format="ttl")
+#         world = owlready2.World()
+#         onto = world.get_ontology(f"file://./_owlready2_input.ttl").load()
+#         t0 = time.time()
+#         owlready2.sync_reasoner(world, infer_property_values =True)
+#         G = world.as_rdflib_graph()
+#         t1 = time.time()
+#         print(f"    owlready2: {data_file} took {t1-t0}")
+#         all_samples['owlready2'][data_file].append({'duration': t1-t0, 'triples': len(G)})
 
 
 # OWLRL
@@ -65,6 +75,23 @@ for data_file in data_files:
         print(f"    owlrl: {data_file} took {t1-t0}")
         all_samples['owlrl'][data_file].append({'duration': t1-t0, 'triples': len(g)})
 
+# Allegro
+from brickschema.inference import OWLRLAllegroInferenceSession
+for data_file in data_files:
+    print(f"Benching owlrl on {data_file}")
+    all_samples['allegro'][data_file] = []
+    for i in range(N):
+        g = rdflib.Graph()
+        load_file(g, data_file)
+        for ont_file in ontology_files:
+            load_file(g, ont_file)
+        sess = OWLRLAllegroInferenceSession()
+
+        t0 = time.time()
+        g = sess.expand(g)
+        t1 = time.time()
+        print(f"    allegro: {data_file} took {t1-t0}")
+        all_samples['allegro'][data_file].append({'duration': t1-t0, 'triples': len(g)})
 
 # reasonable
 import reasonable
@@ -94,3 +121,5 @@ for reasoner, defn in all_samples.items():
             records.append((reasoner, data_file, sample['duration'], sample['triples']))
 df = pd.DataFrame.from_records(records)
 df.columns=['reasoner','data_file','duration','triples']
+df.loc[:, 'src_size'] = df['data_file'].map(lambda x: data_sizes[x])
+df.to_csv('benchmark.csv', header=True, index=False)
