35b7ff1e86e020ab92f1b0baa2c02c65510e28e3
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-10-05 16:47:12 -0600

    benchmarking setup

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion, PlotConfiguration};
-use reasonable::owl::Reasoner;
+use reasonable::reasoner::Reasoner;
+use reasonable::manager::parse_file;
 
 // setup_reasoner!("ontologies/Brick.n3", "small1.n3");
 macro_rules! setup_reasoner {
@@ -54,14 +55,71 @@ fn bench_simple(c: &mut Criterion) {
     group.finish()
 }
 
+fn bench_reload(c: &mut Criterion) {
+    let plot_config = PlotConfiguration::default();
+    let mut group = c.benchmark_group("reload_test");
+    group.plot_config(plot_config);
+
+    group.bench_function("brick_small1", move |b| {
+        b.iter_with_setup(|| setup_reasoner![
+        "example_models/ontologies/Brick.n3",
+        "example_models/small1.n3"
+        ], |mut r| {
+            r.reason();
+            r.load_file("example_models/small1.n3").unwrap();
+            r.reason();
+        })
+    });
+
+    group.bench_function("brick_soda", move |b| {
+        b.iter_with_setup(|| setup_reasoner![
+        "example_models/ontologies/Brick.n3",
+        "example_models/soda_hall.n3"
+        ], |mut r| {
+            r.reason();
+            r.load_file("example_models/soda_hall.n3").unwrap();
+            r.reason();
+        })
+    });
+}
+
+#[allow(dead_code)]
+fn bench_incremental(c: &mut Criterion) {
+    let plot_config = PlotConfiguration::default();
+    let mut group = c.benchmark_group("incremental_reason");
+    group.plot_config(plot_config);
+
+    group.bench_function("brick_small1", move |b| {
+        b.iter_with_setup(|| setup_reasoner![
+        "example_models/ontologies/Brick.n3"
+        ], |mut r| {
+            for t in parse_file("example_models/small1.n3").unwrap() {
+                r.load_triples(vec![t]);
+                r.reason()
+            }
+        })
+    });
+
+    group.bench_function("brick_soda", move |b| {
+        b.iter_with_setup(|| setup_reasoner![
+        "example_models/ontologies/Brick.n3"
+        ], |mut r| {
+            for t in parse_file("example_models/soda_hall.n3").unwrap() {
+                r.load_triples(vec![t]);
+                r.reason()
+            }
+        })
+    });
+}
+
 fn setup() -> Criterion {
-    Criterion::default().sample_size(50).with_plots()
+    Criterion::default().sample_size(10).with_plots()
 }
 
 criterion_group!{
     name=benches;
     config=setup();
-    targets=bench_simple
+    targets=bench_simple, bench_reload
 }
 
 criterion_main!(benches);
