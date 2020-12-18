95f9876f99c5b638da67db4203ac9f877dac9a0e
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-12-17 16:48:42 -0800

    add test.py file

---
+++ test.py
@@ -0,0 +1,16 @@
+import reasonable
+import rdflib
+
+g = rdflib.Graph()
+g.parse('Brick.ttl', format='ttl')
+g.parse('example_models/small1.n3', format='ntriples')
+
+r = reasonable.PyReasoner()
+r.from_graph(g)
+triples = r.reason()
+print("from rdflib:", len(triples))
+g2 = rdflib.Graph()
+for t in triples:
+    print(t)
+    g2.add(t)
+    g2.serialize('output.ttl', format='ttl')
