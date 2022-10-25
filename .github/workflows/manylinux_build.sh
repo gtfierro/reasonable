98725cc0ff48369dd07a9567475bedda078ac64c
Gabe Fierro <gtfierro@mines.edu>
2022-10-25 00:05:01 -0600

    poetry

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -5,7 +5,6 @@ source scl_source enable llvm-toolset-7.0
 curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
 export PATH="${PATH}:/root/.cargo/bin:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 cd python
-python3.10 -m venv venv
-source venv/bin/activate
-pip install -r requirements.dev.txt
-maturin build --release -m Cargo.toml
+curl -sSL https://install.python-poetry.org | python3 -
+poetry install
+poetry run maturin build --release -m Cargo.toml
