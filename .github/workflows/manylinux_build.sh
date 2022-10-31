4e551d4c5a675970de7d80e61504159be3491274
Gabe Fierro <gtfierro@mines.edu>
2022-10-31 07:43:21 -0600

    fix manylinux build

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -4,7 +4,6 @@ yum -y install llvm-toolset-7.0
 source scl_source enable llvm-toolset-7.0
 curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
 export PATH="${PATH}:/root/.cargo/bin:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
-cd python
 python3 -m pip install poetry
 poetry install
-poetry run maturin build --release -m Cargo.toml
+poetry run maturin build --release -m Cargo.toml -F python
