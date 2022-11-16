3c0d5a2b38e705dfb5fe64cecc5fad1c89e60071
Gabe Fierro <gtfierro@mines.edu>
2022-11-16 16:25:09 -0700

    no minimal profile?

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -2,7 +2,7 @@ cd /workdir
 yum -y install centos-release-scl-rh
 yum -y install llvm-toolset-7.0
 source scl_source enable llvm-toolset-7.0
-curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="${PATH}:/root/.cargo/bin:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 python3 -m pip install poetry
 poetry install
