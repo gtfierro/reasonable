4a5f5e1cb1d67d02c253142269304f71f779dcb2
Gabe Fierro <gtfierro@mines.edu>
2022-11-16 16:42:34 -0700

    working in poetry, not pip

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -4,7 +4,7 @@ yum -y install llvm-toolset-7.0
 source scl_source enable llvm-toolset-7.0
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="${PATH}:/root/.cargo/bin:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
-python3 -m pip install poetry
+poetry run pip install poetry
 poetry install
 poetry run maturin build --release -m Cargo.toml -F python
 poetry run python -c "import reasonable"
